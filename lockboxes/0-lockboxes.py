5ba1a58ced9c6726fe011a8e82cf06a1c9354dbc
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-05-06 11:03:32 +0200

    readme

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
+"""
+Write a method that determines if all the boxes can be opened.
+"""
+
 def canUnlockAll(boxes):
+    #excludes last box
     for n in range(len(boxes) - 1):
+        #if a box is empty
         if not boxes[n]:
             return False
     return True
